7396acae6b577823e6527362502f41918ead43a4
RakibulAzadd <rakibul.azad.hstu@example.com>
2025-06-09 00:11:04 +0600

    fmt

---
+++ practice/if_else.go/code.go
@@ -0,0 +1,14 @@
+package main 
+import "fmt"
+func main(){
+	var a,b int = 10,20
+
+	if(a<b){
+		fmt.Printf("%d is less than %d\n", b,a)
+	} else if(a==b) {
+		fmt.Printf("%d is equal to %d\n", b,a)
+	} else {
+		fmt.Printf("%d is greater than %d\n", b,a)
+	}
+	fmt.Println("End of program")
+}
